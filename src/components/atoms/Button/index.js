0fcb00509caaa4c79fd81ce98c9a679e1333d436
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-10-15 15:09:53 +0100

    feat(landing-page): fix all folder structure isses
    - moves files from HOC to compounds

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-const Button = (props) => {
-  const { text, green } = props;
-  return (
-    <button type="button" className={`App__Button ${green ? 'green' : 'white'} `}>
-      {text}
-    </button>
-  );
-};
+const Button = ({ text, green }) => (
+  <button type="button" className={`App__Button ${green ? 'green' : 'white'} `}>
+    {text}
+  </button>
+);
 
 Button.propTypes = {
   green: PropTypes.bool,
